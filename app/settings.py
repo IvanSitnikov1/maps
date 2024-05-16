6ae612a9e2885c173ee2ab38770d65fbbe555ee3
IvanSitnikov1 <iw.sitnikoff@yandex.ru>
2024-05-16 09:48:49 +0300

    deploy

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pwlh737tv42volrjmt9!+o(tz%vyonjyb89((-4$!t8(bon@(!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
